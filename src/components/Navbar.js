c9f41bc6b5b1b9dccd7d8444913160e24a36af80
phillipug <phillipmusiime@gmail.com>
2020-12-15 21:07:30 +0300

    add navbar component plus styles

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import '../assets/styles/Navbar.css'
+import logo from '../assets/images/icon.svg'
+import { Button } from './Button'
+import { navBtnFive, navBtnFour, navBtnOne, navBtnSix, navBtnThree, navBtnTwo } from './Data'
+import { FiSearch } from 'react-icons/fi'
+import { BsStar } from 'react-icons/bs'
+import { IconContext } from 'react-icons'
+
+function Navbar() {
+  return (
+    <IconContext.Provider value={{ color: '#AAAAAA', size: 20 }}>
+      <div className="navbar">
+        <div>
+          <img src={logo} alt="logo" />
+          <div>
+            <a href="#!">Training Mode</a>
+            <a href="#!">Live Mode</a>
+          </div>
+        </div>
+        <div>
+          <Button {...navBtnOne} />
+          <Button {...navBtnTwo} />
+          <Button {...navBtnThree} />
+          <Button {...navBtnFour} />
+          <Button {...navBtnFive} />
+          <Button {...navBtnSix} />
+        </div>
+        <div>
+          <FiSearch />
+          <BsStar />
+          <BsStar />
+          <div className="user">
+            <span>12</span>
+          </div>
+        </div>
+      </div>
+    </IconContext.Provider>
+  )
+}
+
+export default Navbar
