8654912f22658d1d608b08f2afa3906554518fea
phillipug <phillipmusiime@gmail.com>
2020-12-15 21:05:55 +0300

    separate data for navbar buttons

---
+++ src/components/Data.js
@@ -0,0 +1,37 @@
+export const navBtnOne = {
+  children: "Show All",
+  type: "",
+  buttonStyle: "btn--outline",
+}
+
+export const navBtnTwo = {
+  children: "Crypto",
+  type: "",
+  buttonColor: 'white',
+  buttonStyle: "btn--outline",
+}
+
+export const navBtnThree = {
+  children: "Commodities",
+  type: "",
+  buttonStyle: "btn--outline",
+}
+
+export const navBtnFour = {
+  children: "Stock",
+  type: "",
+  buttonColor: 'white',
+  buttonStyle: "btn--outline",
+}
+
+export const navBtnFive = {
+  children: "Index",
+  type: "",
+  buttonStyle: "btn--outline",
+}
+
+export const navBtnSix = {
+  children: "Currency",
+  type: "",
+  buttonStyle: "btn--outline",
+}
