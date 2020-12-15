827e637d63d71dc35d63fd0267df0ffc12aa880f
phillipug <phillipmusiime@gmail.com>
2020-12-15 21:10:16 +0300

    import fonts into index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/styles/index.css';
 import App from './App';
+import './assets/fonts/Roboto/Roboto-Light.ttf'
+import './assets/fonts/Roboto/Roboto-Medium.ttf'
+import './assets/fonts/Roboto/Roboto-Regular.ttf'
+import './assets/fonts/Roboto/Roboto-Thin.ttf'
 
 ReactDOM.render(
   <App />,
