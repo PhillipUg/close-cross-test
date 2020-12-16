9b9bc8e814bc7e8d41c45a5df67dfc92b2bc7b97
phillipug <phillipmusiime@gmail.com>
2020-12-16 11:35:14 +0300

    add data for dataStrip component

---
+++ src/components/pages/explore/Data.js
@@ -0,0 +1,17 @@
+export const stripOne = {
+  ethHours: '24',
+  amount: '33,452.00',
+  units: '+32.5%',
+  outcomeTimeLabel: 'Outcome in 5h 23m',
+  color: 'green',
+  rate: '2,3x'
+}
+
+export const stripTwo = {
+  ethHours: '48',
+  amount: '52.00',
+  units: '-1.5%',
+  outcomeTimeLabel: 'Outcome in 2h 23m',
+  color: 'red',
+  rate: '1,7x'
+}
