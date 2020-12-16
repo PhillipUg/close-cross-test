cb394457094781d8380489240a6609131efd6c0b
phillipug <phillipmusiime@gmail.com>
2020-12-16 11:37:19 +0300

    refactor button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,10 @@ import '../assets/styles/Button.css'
 
 const STYLES = ['btn--primary', 'btn--outline']
 
-// const SIZES = ['btn--medium', 'btn--large', 'btn--mobile']
-
-const COLOR = ['primary', 'white', 'red', 'green']
+const COLOR = ['primary', 'white', 'green', 'green--outline']
 
 export const Button = ({ children, type, onClick, buttonStyle, buttonColor }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
-  // const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
   const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null
   return (
     <button className={`btn ${checkButtonStyle} ${checkButtonColor}`} onClick={onClick} type={type}>{children}</button>
